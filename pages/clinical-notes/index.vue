13b68d8eede78caa53e30c3f0d0a814b176bc32e
Jamie Archibald <jamie3@gmail.com>
2025-12-04 15:14:41 -0400

    Changed column name

--- pages/clinical-notes/index.vue
+++ pages/clinical-notes/index.vue
@@ -51,7 +51,7 @@
               scope="col"
               class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
             >
-              Note Date
+              Session Date
             </th>
             <th
               scope="col"
